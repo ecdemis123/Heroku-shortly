2a910ee9ba96787ee581825f5fc6579ffa2dac2e
MKS-students <mks.students@hackreactor.com>
2016-02-12 17:31:57 -0800

    fixed again

--- client/dist/built.js
+++ client/dist/built.js
@@ -63,11 +63,11 @@ angular.module('shortly', [
   // when it does change routes, we then look for the token in localstorage
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
-  $rootScope.$on('$routeChangeStart',['evt', 'next', 'current', function (evt, next, current) {
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/signin');
     }
-  }]);
+  });
 }]);
 
 // do not tamper with this code in here, study it, but do not touch
